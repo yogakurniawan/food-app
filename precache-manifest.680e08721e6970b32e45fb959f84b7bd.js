702a630e922a731d3a2059bab4099a98029894be
Yoga Kurniawan <yoga.kwn@gmail.com>
2019-10-15 14:47:48 +0700

    Updates

---
+++ precache-manifest.680e08721e6970b32e45fb959f84b7bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e440e5356f9b660836be2d029c79b847",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e284107061a272dd74b",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c8db9247ef7f4e28e3e9",
+    "url": "/static/js/2.0e9f0d41.chunk.js"
+  },
+  {
+    "revision": "4e284107061a272dd74b",
+    "url": "/static/js/main.71a0272b.chunk.js"
+  },
+  {
+    "revision": "329272f44081637c08cb",
+    "url": "/static/js/runtime-main.94fc7c18.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
