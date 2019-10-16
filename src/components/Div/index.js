a188ae582ba28d09b1ba7578e1213552eafc4aef
Yoga Kurniawan <yoga.kwn@gmail.com>
2019-10-17 01:33:48 +0700

    added Div as a common component

---
+++ src/components/Div/index.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import { space, position, width, height } from 'styled-system'
+
+export const Div = styled.div`
+  ${space}
+  ${position}
+  ${width}
+  ${height}
+`
+
+export default Div
